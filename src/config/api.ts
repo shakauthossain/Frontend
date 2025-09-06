b243d9e351e3a2f7e730acf23aa9481eeed1312a
shakauthossain <shakauthossain0@gmail.com>
2025-09-06 13:12:43 +0600

    Update docker

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://notionhive-ai-nh-outreach-agent.hf.space";
-// export const API_BASE_URL = "http://localhost:8000";
+// export const API_BASE_URL = "https://notionhive-ai-nh-outreach-agent.hf.space";
+export const API_BASE_URL = "http://localhost:8000";
